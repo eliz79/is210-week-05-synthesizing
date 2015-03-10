d78e07edcc9b00c5c72daa919aa9dfe50cfe372b
Erica Liz <erica.liz@spsmail.cuny.edu>
2015-03-10 23:51:23 +0000

    Corrected typo

--- task_01.py
+++ task_01.py
@@ -38,12 +38,12 @@ def celsius_to_kelvin(degrees):
         >>>celsius_to_kelvin(100)
         Decimal('373.15')
     """
-    return (decimal.Decimal(degrees + ABSOLUTE_DIFFERENCE))
+    return celsius_to_kelvin(fahrenheit_to_celsius(degrees))
 
 
-def farenheit_to_kelvin(degrees):
+def fahrenheit_to_kelvin(degrees):
 
-    """A function that will convert degrees from Farenheit to Kelvin
+    """A function that will convert degrees from Fahrenheit to Kelvin
 
     Arg:
         degree(int): value that will be used to convert from F to K
@@ -52,10 +52,10 @@ def farenheit_to_kelvin(degrees):
         Convert the temperature from F to K
 
     Example:
-        >>>farenheit_to_kelvin(212)
+        >>>fahrenheit_to_kelvin(212)
         Decimal('373.15')
         """
-    return farenheit_to_celsius(degrees) + ABSOLUTE_DIFFERENCE
+    return fahrenheit_to_celsius(degrees) + ABSOLUTE_DIFFERENCE
 
-F_TO_K = farenheit_to_kelvin(212) + ABSOLUTE_DIFFERENCE
+F_TO_K = fahrenheit_to_kelvin(212) + ABSOLUTE_DIFFERENCE
 print F_TO_K
